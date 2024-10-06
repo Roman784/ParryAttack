f73d7bb6970e527cba3ac1a8b08cc5a42714f910
Roman784 <rmironenko50@gmail.com>
2024-10-06 14:06:55 +0300

    Swordsman movement

--- Assets/_Project/Develop/Gameplay/Swordsman/SwordsmanPositioning.cs
+++ Assets/_Project/Develop/Gameplay/Swordsman/SwordsmanPositioning.cs
@@ -1,24 +1,39 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class SwordsmanPositioning : MonoBehaviour
 {
     [SerializeField, Range(-1, 1)] private int _forwardMotionStep;
+    [SerializeField] private AnimationCurve _moveSpeedOverTime;
+    [SerializeField] private float _moveSpeed;
 
     private ArenaPositions _arenaPositions;
     private Vector2 _arenaPosition;
 
-    public UnityEvent<int> OnMovedBack = new();
+    [HideInInspector] public UnityEvent<int> OnMovedBack = new();
+
+    private Coroutine _smoothlyMove;
 
     public void Init(ArenaPositions arenaPositions)
     {
         _arenaPositions = arenaPositions;
     }
 
-    public void SetPosition(Vector2 position)
+    public void SetPosition(Vector2 position, bool isInstantly = true)
     {
         _arenaPosition = position;
-        transform.position = position;
+
+        if (isInstantly)
+        {
+            transform.position = position;
+            return;
+        }
+
+        if (_smoothlyMove != null)
+            Coroutines.StopRoutine(_smoothlyMove);
+
+        _smoothlyMove = Coroutines.StartRoutine(MoveSmoothly(position));
     }
 
     public void MoveBack()
@@ -29,12 +44,28 @@ public class SwordsmanPositioning : MonoBehaviour
 
     public void Move(int step)
     {
-        Debug.Log("aaaaaa");
         int newPositionIndex = _arenaPositions.GetIndexByPosition(_arenaPosition) + step;
 
         if (!_arenaPositions.IsWithin(newPositionIndex)) return;
 
         Vector2 position = _arenaPositions.GetPosition(newPositionIndex);
-        SetPosition(position);
+        SetPosition(position, false);
+    }
+
+    private IEnumerator MoveSmoothly(Vector2 position)
+    {
+        float initialDistance = Mathf.Abs(position.x - transform.position.x);
+
+        do
+        {
+            float distance = Mathf.Abs(position.x - transform.position.x);
+            float progress = 1f - (distance / initialDistance);
+            float speed = _moveSpeed * _moveSpeedOverTime.Evaluate(progress);
+
+            transform.position = Vector2.MoveTowards(transform.position, position, speed * Time.deltaTime);
+
+            yield return null;
+        }
+        while (Vector2.Distance(transform.position, position) > 0.05f);
     }
 }
