1d5dfe28203fbc79fa022cab5f5b1ea09cbb85ea
Roman784 <rmironenko50@gmail.com>
2024-10-24 09:24:51 +0300

    Fixed automatic level list scrolling

--- Assets/_Project/Develop/UI/Root/LevelListUI.cs
+++ Assets/_Project/Develop/UI/Root/LevelListUI.cs
@@ -1,16 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 using Zenject;
 
 public class LevelListUI : MonoBehaviour
 {
     [SerializeField] private LevelSelectionButton _selectionButtonPrefab;
+    [SerializeField] private ScrollRect _scrollRect;
     [SerializeField] private Transform _selectionButtonContainer;
 
     public LevelSelectionButton CreateButton()
     {
         var button = Instantiate(_selectionButtonPrefab);
         button.transform.SetParent(_selectionButtonContainer, false);
+
+        _scrollRect.verticalNormalizedPosition = 1;
 
         return button;
     }
